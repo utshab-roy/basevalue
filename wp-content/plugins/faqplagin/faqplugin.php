d69ad63bfa34f659fa87162278e201ca6dbcceae
Utshab Roy <utshab.roy@gmail.com>
2018-09-19 19:11:51 +0600

    color, date, time picker, settings API and many more field added

--- wp-content/plugins/faqplagin/faqplugin.php
+++ wp-content/plugins/faqplagin/faqplugin.php
@@ -24,6 +24,9 @@ class CBXFaq{
 
 	    add_action( 'wp_enqueue_scripts', array($this, 'basevalue_scripts') );
 
+	    //adding JS file to admin panel
+	    add_action( 'admin_enqueue_scripts', array($this, 'basevalue_admin_script') );
+
 	    /**
 	     * register our wporg_options_page to the admin_menu action hook
 	     */
@@ -88,9 +91,41 @@ class CBXFaq{
 	}
 
 	// Add scripts and stylesheets
-	function basevalue_scripts($hook) {
-        var_dump($hook);
+	function basevalue_scripts() {
+//        var_dump($hook);
 		wp_enqueue_script( 'main', plugin_dir_url( __FILE__ ). 'main.js', array( 'jquery' ), '3.3.6', true );
+	}
+
+	// Add scripts and css for admin panel
+	function basevalue_admin_script($hook){
+//		var_dump($hook);
+//        die();
+		if ($hook != 'toplevel_page_wporg'){
+			return;
+		}
+		wp_enqueue_script('main', plugin_dir_url( __FILE__ ). 'main.js', array('jquery', 'wp-color-picker'), '1.0.0', true);
+
+		wp_enqueue_script( 'jquery-ui-datepicker' );
+
+
+		wp_register_script( 'timepicker_js', 'https://cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.js' );
+		wp_enqueue_script('timepicker_js');
+
+		wp_register_style( 'timepicker_style', 'https://cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.css' );
+		wp_enqueue_style('timepicker_style');
+
+		wp_register_style( 'jquery-ui-css', 'https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css' );
+		wp_enqueue_style('jquery-ui-css');
+
+
+		wp_enqueue_script('date-time-js', plugin_dir_url( __FILE__ ). 'jquery-ui-timepicker-addon.min.js', array('jquery'), '1.0.0', true);
+
+		wp_register_style('date-time-css', plugin_dir_url( __FILE__ ). 'jquery-ui-timepicker-addon.min.css');
+		wp_enqueue_style('date-time-css');
+
+		//for color picker
+		wp_enqueue_style( 'wp-color-picker' );
+        // the script dependencies has been added to the main.js file. so that we can use color-picker
 	}
 
 
@@ -132,6 +167,7 @@ class CBXFaq{
 // the "label_for" key value is used for the "for" attribute of the <label>.
 // the "class" key value is used for the "class" attribute of the <tr> containing the field.
 // you can add custom key value pairs to be used inside your callbacks.
+        //dropdown
 		function wporg_field_pill_cb( $args ) {
 			// get the value of the setting we've registered with register_setting()
 			$options = get_option( 'wporg_options' );
@@ -157,7 +193,7 @@ class CBXFaq{
             </p>
 			<?php
 		}
-
+        //text input
 		function wporg_field_name_cb( $args ) {
 			$options = get_option( 'wporg_options' );
 
@@ -170,7 +206,7 @@ class CBXFaq{
 
 			<?php
 		}
-
+        //multiple checkbox
 		function wporg_field_places_cb( $args ) {
 			$options = get_option( 'wporg_options' );
 
@@ -178,7 +214,9 @@ class CBXFaq{
 
 
 			echo '<pre>';
-			print_r( $wporg_field_place );
+//			print_r( $wporg_field_place );
+//			var_dump(my_get_option('radio','my_settings',''));
+//			var_dump(get_option('wporg_options'));
 			echo '</pre>';
 
 
@@ -201,6 +239,109 @@ class CBXFaq{
 
 			<?php
 		}
+        //textarea
+		function wporg_field_blog_cb( $args ) {
+			$options = get_option( 'wporg_options' );
+
+//			echo '<pre>';
+//			var_dump($options[ $args['label_for'] ]);
+//			echo '</pre>';
+			?>
+            <textarea id="<?php echo esc_attr( $args['label_for'] ); ?>" name="wporg_options[<?php echo esc_attr( $args['label_for'] ); ?>]>" rows="4" cols="50"><?php  esc_attr_e(trim($options[ $args['label_for'] ])); ?></textarea>
+
+			<?php
+		}
+        //number field
+		function wporg_field_age_cb( $args ) {
+			$options = get_option( 'wporg_options' );
+
+//			echo '<pre>';
+//			var_dump($options[ $args['label_for'] ]);
+//			echo '</pre>';
+			?>
+            <input type="number" id="<?php echo esc_attr( $args['label_for'] ); ?>"
+                   data-custom="<?php echo esc_attr( $args['wporg_custom_data'] ); ?>"
+                   name="wporg_options[<?php echo esc_attr( $args['label_for'] ); ?>]>"
+                   value="<?php echo esc_attr( $options[ $args['label_for'] ] ); ?>"/>
+
+			<?php
+		}
+		//radio button
+		function wporg_field_shift_cb( $args ) {
+			// get the value of the setting we've registered with register_setting()
+			$options = get_option( 'wporg_options' );
+//            echo '<pre>';
+//			var_dump($options[ $args['label_for'] ]);
+//			echo '</pre>';
+			?>
+            <input type="radio" name="wporg_options[<?php echo esc_attr( $args['label_for'] ); ?>]" id="wporg_options[<?php echo esc_attr( $args['label_for'] ); ?>]-day" value="day" <?php echo isset( $options[ $args['label_for'] ] ) ? ( checked( $options[ $args['label_for'] ], 'day') ) : ( '' ); ?>/>
+            <label for="wporg_options[<?php echo esc_attr( $args['label_for'] ); ?>]-day">Day</label>
+            <br/>
+            <input type="radio" name="wporg_options[<?php echo esc_attr( $args['label_for'] ); ?>]" id="wporg_options[<?php echo esc_attr( $args['label_for'] ); ?>]-night" value="night" <?php echo isset( $options[ $args['label_for'] ] ) ? ( checked( $options[ $args['label_for'] ], 'night' ) ) : ( '' ); ?>/>
+            <label for="wporg_options[<?php echo esc_attr( $args['label_for'] ); ?>]-night">Night</label>
+
+			<?php
+		}
+		//color field
+		function wporg_field_color_cb( $args ) {
+			$options = get_option( 'wporg_options' );
+
+//            echo '<pre>';
+//			var_dump($options[ $args['label_for'] ]);
+//			echo '</pre>';
+			?>
+
+            <input type="text" id="<?php echo esc_attr( $args['label_for'] ); ?>"
+                   name="wporg_options[<?php echo esc_attr( $args['label_for'] ); ?>]>"
+                   value="<?php echo esc_attr( $options[ $args['label_for'] ] ); ?>"/>
+			<?php
+		}
+		//date-picker using jQuery UI
+		function wporg_field_date_cb( $args ) {
+			$options = get_option( 'wporg_options' );
+
+//            echo '<pre>';
+//			var_dump($options[ $args['label_for'] ]);
+//			echo '</pre>';
+			?>
+            <input type="text" id="<?php echo esc_attr( $args['label_for'] ); ?>"
+                   data-custom="<?php echo esc_attr( $args['wporg_custom_data'] ); ?>"
+                   name="wporg_options[<?php echo esc_attr( $args['label_for'] ); ?>]>"
+                   value="<?php if (isset($options[ $args['label_for'] ])) echo esc_attr( $options[ $args['label_for'] ] ); ?>"/>
+
+			<?php
+		}
+		//time-picker using timepicker.js
+		function wporg_field_time_cb( $args ) {
+			$options = get_option( 'wporg_options' );
+
+//            echo '<pre>';
+//			var_dump($options[ $args['label_for'] ]);
+//			echo '</pre>';
+			?>
+            <input type="text" id="<?php echo esc_attr( $args['label_for'] ); ?>"
+                   data-custom="<?php echo esc_attr( $args['wporg_custom_data'] ); ?>"
+                   name="wporg_options[<?php echo esc_attr( $args['label_for'] ); ?>]>"
+                   value="<?php if (isset($options[ $args['label_for'] ])) echo esc_attr( $options[ $args['label_for'] ] ); ?>"/>
+
+			<?php
+		}
+
+		//time-picker using date-time picker
+		function wporg_field_datetime_cb( $args ) {
+			$options = get_option( 'wporg_options' );
+
+//            echo '<pre>';
+//			var_dump($options[ $args['label_for'] ]);
+//			echo '</pre>';
+			?>
+            <input type="text" id="<?php echo esc_attr( $args['label_for'] ); ?>"
+                   data-custom="<?php echo esc_attr( $args['wporg_custom_data'] ); ?>"
+                   name="wporg_options[<?php echo esc_attr( $args['label_for'] ); ?>]>"
+                   value="<?php if (isset($options[ $args['label_for'] ])) echo esc_attr( $options[ $args['label_for'] ] ); ?>"/>
+
+			<?php
+		}
 
 	}
 
@@ -220,6 +361,7 @@ class CBXFaq{
 		);
 
 		// register a new field in the "wporg_section_developers" section, inside the "wporg" page
+        //input drop-down
 		add_settings_field(
 			'wporg_field_pill', // as of WP 4.6 this value is used only internally
 			// use $args' label_for to populate the id inside the callback
@@ -234,6 +376,7 @@ class CBXFaq{
 			]
 		);
 
+		//add settings for input text fiend
 		add_settings_field(
 			'wporg_field_name', // as of WP 4.6 this value is used only internally
 			// use $args' label_for to populate the id inside the callback
@@ -259,6 +402,108 @@ class CBXFaq{
 			[
 				'label_for'         => 'wporg_field_place',
 				'class'             => 'wporg_row'
+			]
+		);
+
+		//add settings for textarea for blog
+		add_settings_field(
+			'wporg_field_blog', // as of WP 4.6 this value is used only internally
+			// use $args' label_for to populate the id inside the callback
+			__( 'Blog', 'wporg' ),
+			'wporg_field_blog_cb',
+			'wporg',
+			'wporg_section_developers',
+			[
+				'label_for'         => 'wporg_field_blog',
+				'class'             => 'wporg_row'
+			]
+		);
+
+		//add settings for number field for age
+		add_settings_field(
+			'wporg_field_age', // as of WP 4.6 this value is used only internally
+			// use $args' label_for to populate the id inside the callback
+			__( 'Age', 'wporg' ),
+			'wporg_field_age_cb',
+			'wporg',
+			'wporg_section_developers',
+			[
+				'label_for'         => 'wporg_field_age',
+				'class'             => 'wporg_row'
+			]
+		);
+
+		//add settings radio input for shift of work
+		add_settings_field(
+			'wporg_field_shift', // as of WP 4.6 this value is used only internally
+			// use $args' label_for to populate the id inside the callback
+			__( 'Shift of work', 'wporg' ),
+			'wporg_field_shift_cb',
+			'wporg',
+			'wporg_section_developers',
+			[
+				'label_for'         => 'wporg_field_shift',
+				'class'             => 'wporg_row'
+			]
+		);
+
+		//add setting for color input
+		add_settings_field(
+			'wporg_field_color', // as of WP 4.6 this value is used only internally
+			// use $args' label_for to populate the id inside the callback
+			__( 'Favorite Color', 'wporg' ),
+			'wporg_field_color_cb',
+			'wporg',
+			'wporg_section_developers',
+			[
+				'label_for'         => 'wporg_field_color',
+				'class'             => 'wporg_row',
+				'wporg_custom_data' => 'custom color field',
+			]
+		);
+
+		//add setting for date input
+		add_settings_field(
+			'wporg_field_date', // as of WP 4.6 this value is used only internally
+			// use $args' label_for to populate the id inside the callback
+			__( 'Date', 'wporg' ),
+			'wporg_field_date_cb',
+			'wporg',
+			'wporg_section_developers',
+			[
+				'label_for'         => 'wporg_field_date',
+				'class'             => 'wporg_row',
+				'wporg_custom_data' => 'custom name field',
+			]
+		);
+
+		//add setting for time input
+		add_settings_field(
+			'wporg_field_time', // as of WP 4.6 this value is used only internally
+			// use $args' label_for to populate the id inside the callback
+			__( 'Time', 'wporg' ),
+			'wporg_field_time_cb',
+			'wporg',
+			'wporg_section_developers',
+			[
+				'label_for'         => 'wporg_field_time',
+				'class'             => 'wporg_row',
+				'wporg_custom_data' => 'custom time field',
+			]
+		);
+
+		//add setting for date-time input
+		add_settings_field(
+			'wporg_field_datetime', // as of WP 4.6 this value is used only internally
+			// use $args' label_for to populate the id inside the callback
+			__( 'Date and Time', 'wporg' ),
+			'wporg_field_datetime_cb',
+			'wporg',
+			'wporg_section_developers',
+			[
+				'label_for'         => 'wporg_field_datetime',
+				'class'             => 'wporg_row',
+				'wporg_custom_data' => 'custom datetime field',
 			]
 		);
 	}
@@ -302,6 +547,8 @@ class CBXFaq{
 		<?php
 	}
 
+
+
 }//end of CBXFaq class
 
 
@@ -315,4 +562,18 @@ function cbxfaq_load_plugin() {
 }
 
 add_action( 'plugins_loaded', 'cbxfaq_load_plugin', 5);
+
+function my_get_option( $option, $section, $default = '' ) {
+
+	$options = get_option( $section );
+
+	if ( isset( $options[$option] ) ) {
+		return $options[$option];
+	}
+
+	return $default;
+}
+
+
+//die();
 
--- wp-content/plugins/faqplagin/main.js
+++ wp-content/plugins/faqplagin/main.js
@@ -7,5 +7,29 @@ jQuery(document).ready(function ($) {
 
     });
 
+    var $wporg_field_date = $('#wporg_field_date');
+
+    $wporg_field_date.datepicker({
+        dateFormat : 'dd-mm-yy',
+        showOtherMonths: true,
+        selectOtherMonths: true,
+        orientation: "top"
+    });
+
+    var $wporg_field_time = $('#wporg_field_time');
+    $wporg_field_time.timepicker({});
+
+    var $wporg_field_datetime = $('#wporg_field_datetime');
+    $wporg_field_datetime.datetimepicker({
+        dateFormat: 'dd-mm-yy',
+        timeFormat: 'hh:mm:ss tt'
+    });
+
+    var $wporg_field_color = $('#wporg_field_color');
+    $wporg_field_color.wpColorPicker({});
+
+
+
+
 
 });//end of jQuery ready function
